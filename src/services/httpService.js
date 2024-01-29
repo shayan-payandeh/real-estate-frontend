fb0928c57c716f6b0d07222784adf355fa9a5b9f
Shayan Payandeh <shayan.iker@yahoo.com>
2024-01-29 20:12:42 +0330

    change baseurl

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const app = axios.create({
-  baseURL: 'https://api.hormozganfile.info/',
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
 });
 
@@ -18,7 +18,7 @@ app.interceptors.response.use(
       originalConfig._retry = true;
       try {
         const { data } = await axios.get(
-          `${process.env.NEXT_PUBLIC_API_URL}/user/refresh-token`,
+          `${process.env.NEXT_PUBLIC_API_URL}user/refresh-token`,
           { withCredentials: true }
         );
         if (data) return app(originalConfig);
