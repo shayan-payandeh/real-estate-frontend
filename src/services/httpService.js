dc2beaef3aaa3fca62c55a009c195ed139e8752e
Shayan Payandeh <shayan.iker@yahoo.com>
2024-01-30 00:15:29 +0330

    change baseurl

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -10,25 +10,25 @@ app.interceptors.request.use(
   (err) => Promise.reject(err)
 );
 
-app.interceptors.response.use(
-  (res) => res,
-  async (err) => {
-    const originalConfig = err.config;
-    if (err.response.status === 401 && !originalConfig._retry) {
-      originalConfig._retry = true;
-      try {
-        const { data } = await axios.get(
-          `${process.env.NEXT_PUBLIC_API_URL}user/refresh-token`,
-          { withCredentials: true }
-        );
-        if (data) return app(originalConfig);
-      } catch (error) {
-        return Promise.reject(error);
-      }
-    }
-    return Promise.reject(err);
-  }
-);
+// app.interceptors.response.use(
+//   (res) => res,
+//   async (err) => {
+//     const originalConfig = err.config;
+//     if (err.response.status === 401 && !originalConfig._retry) {
+//       originalConfig._retry = true;
+//       try {
+//         const { data } = await axios.get(
+//           `${process.env.NEXT_PUBLIC_API_URL}user/refresh-token`,
+//           { withCredentials: true }
+//         );
+//         if (data) return app(originalConfig);
+//       } catch (error) {
+//         return Promise.reject(error);
+//       }
+//     }
+//     return Promise.reject(err);
+//   }
+// );
 
 const http = {
   get: app.get,
